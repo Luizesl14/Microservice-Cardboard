f47cfee3452123e8d98507e623985472fdac142d
Luizesl <luiz@bluetechno.com.br>
2022-01-24 01:14:16 -0300

    all services working together

--- system-order-service/src/main/java/com/systemorderservice/SystemOrderServiceApplication.java
+++ system-order-service/src/main/java/com/systemorderservice/SystemOrderServiceApplication.java
@@ -2,9 +2,11 @@ package com.systemorderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class SystemOrderServiceApplication {
 
     public static void main(String[] args) {
